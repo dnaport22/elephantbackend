73d662d767ad376d1c34b846be27505d32467ee9
deep-nav <navdeep.dhuti@cameronandwilding.com>
2016-12-24 21:14:13 +0000

    Updated index.php

--- index.php
+++ index.php
@@ -4,8 +4,5 @@
  * Redirect to the main app view.
  */
 
-$url = 'http://' . $_SERVER['HTTP_HOST'] . '/WebApp/www/index.html';
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-header("HTTP/1.1 301 Moved Permanently");
-header("Location: $url");
