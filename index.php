e7082f95c4fdbb890c741fef239e32b48c3e6595
deep-nav <navdeep.dhuti@cameronandwilding.com>
2016-12-24 15:10:40 +0000

    Testing server

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
  * Redirect to the main app view.
  */
 
-$url = 'http://' . $_SERVER['HTTP_HOST'] . '/WebApp/www/index.html';
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-header("HTTP/1.1 301 Moved Permanently");
-header("Location: $url");
+//$url = 'http://' . $_SERVER['HTTP_HOST'] . '/WebApp/www/index.html';
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+// header("HTTP/1.1 301 Moved Permanently");
+
+echo 'Worked!!';
+//header("Location: $url");
