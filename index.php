7cb05d2d0cec6df4b953a1485b7eb2076c4a56a7
deep-nav <navdeep.dhuti@cameronandwilding.com>
2016-12-24 21:15:11 +0000

    Updated index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
-//$url = 'http://' . $_SERVER['HTTP_HOST'] . '/WebApp/www/index.html';
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-// header("HTTP/1.1 301 Moved Permanently");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 
-echo 'Worked!!';
-//header("Location: $url");
+
