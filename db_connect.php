55f4ed986e5b4b0c5d7122b394071a8de8c223a2
deep-nav <navdeep.dhuti@cameronandwilding.com>
2016-12-25 11:31:44 +0000

    Testing db credentials method

--- db_connect.php
+++ db_connect.php
@@ -53,7 +53,7 @@ class dbConnect
 	 * dbConnect constructor.
 	 */
 	public function __construct() {
-		print_r (getDbCredentials())
+		print_r ($this->getDbCredentials());
 		$this->load(parse_ini_file("../../config/config.ini", TRUE));
 		$this->connection = new PDO("mysql:host=$this->hostname;dbname=$this->dbname", $this->username, $this->password);
 		$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
