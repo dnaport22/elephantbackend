65408d31bfb7ee76cb5d2364d7316fd37f8113c8
deep-nav <navdeep.dhuti@cameronandwilding.com>
2017-01-10 12:15:53 +0000

    Updating elephant url

--- settings.php
+++ settings.php
@@ -6,12 +6,12 @@
 
 class Settings 
 {
-	/**
-	 * Server urls.
-	 *
-	 * @var string
-	 */
-	private $live_server = "service.myelephant.xyz";
+  /**
+   * Server urls.
+   *
+   * @var string
+   */
+  private $live_server = "service.myelephant.xyz";
   private $dev_server = "develop.myelephant.xyz";
   private $test_server = "test.myelephant.xyz";
   private $dev_mode = false;
@@ -26,9 +26,9 @@ class Settings
    * @return string 
    */
   public function getServerUrl($location) {
-  	if ($location === $this->live_server) {
-  		return 'myelephant.xyz';
-  	}
+    if ($location === $this->live_server) {
+      return 'myelephant.xyz/webapp';
+    }
     elseif ($location === $this->dev_server) {
       return 'developweb.myelephant.xyz';
     }
@@ -38,42 +38,42 @@ class Settings
   }
 
   /**
-	 * Get credentials for server type, i.e. develop, test, live
-	 * 
-	 * @param string $server
-	 * 	  Current server.
-	 * 
-	 * @return string db_credentials
-	 */
+   * Get credentials for server type, i.e. develop, test, live
+   * 
+   * @param string $server
+   *    Current server.
+   * 
+   * @return string db_credentials
+   */
   public function getDbCredentials($location) {
-  	if(!$this->dev_mode) {
-  		if ($location === $this->live_server) {
-  			return 'db_credentials_live';
-  		}
-			elseif ($location === $this->dev_server) {
-				return 'db_credentials_dev';
-			}
-			elseif ($location === $this->test_server) {
-				return 'db_credentials_test';
-			}
-  	}
-  	else {
-  		return 'db_credentials_local';
-  	}
-  	
+    if(!$this->dev_mode) {
+      if ($location === $this->live_server) {
+        return 'db_credentials_live';
+      }
+      elseif ($location === $this->dev_server) {
+        return 'db_credentials_dev';
+      }
+      elseif ($location === $this->test_server) {
+        return 'db_credentials_test';
+      }
+    }
+    else {
+      return 'db_credentials_local';
+    }
+    
   }
 
   public function setDevMode($mode) {
-  	if (!$mode) {
-  		$this->dev_mode = false;
-  	}
-  	else {
-  		$this->dev_mode = true;
-  	}
+    if (!$mode) {
+      $this->dev_mode = false;
+    }
+    else {
+      $this->dev_mode = true;
+    }
   }
 
   public function setCreLocation($new_location) {
-  	$this->cre_location = $new_location;
+    $this->cre_location = $new_location;
   }
   
 }
