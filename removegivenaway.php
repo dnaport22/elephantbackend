53f7df84afc097ab7a55e0ecdf4a6da3989ba7a0
skorekm <skorekmarcin@gmail.com>
2017-03-04 19:09:22 +0000

    when delete item from givenaway it will have status -2

---
+++ removegivenaway.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'db_connect.php';
+require_once 'user.php';
+require_once 'item.php';
+require_once 'response.php';
+
+$user = User::authorize();
+$item = new Item($mysql_db);
+$item->loadByItemId(@$_POST['itemId']);
+if ($item->authorize($user)) {
+	$item->setStatus(-2);
+	$item->save();
+	Response::flush(1, 'Given away item deleted');
+}
